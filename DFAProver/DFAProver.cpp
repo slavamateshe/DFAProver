a046eb13c67f819036a566b408ba44f9d8dd99ca
Ivan Baranov <slavamateshe@yandex.ru>
2024-03-15 12:22:37 +0300

    nfa is dfa ready

--- DFAProver/DFAProver.cpp
+++ DFAProver/DFAProver.cpp
@@ -5,20 +5,9 @@
 using namespace std;
 
 
-/*nfa* intersect_nfa(nfa* n1, nfa* n2) {
-	node* new_start = NULL;
-	node* new_end = NULL;
-	int c1, c2, q1, q2;
-
-
-}*/
-
 int main()
 {
-	nfa* a = nfa_read("3div.txt");
-	//nfa_to_dot(a, "3div.dot");
-	//int x;
-	//cin >> x;
-	for(int i=0; i < 100; i++)
-	 if (nfa_check(a, i)) cout << i << endl;
+    nfa* a = nfa_read("3div.txt");
+	for (int i = 0; i < 100; i++)
+		if (nfa_check(a, i)) cout << i << endl;
 }
