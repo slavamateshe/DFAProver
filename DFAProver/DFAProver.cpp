71f476dbfcccebe683ea0cf4bc0b077ac1abfd2b
Ikbal <khasas2005@gmail.com>
2024-03-28 18:49:25 +0300

    new file:   2_2div.txt 	modified:   2div.txt 	new file:   6div.txt 	modified:   DFAProver.cpp 	modified:   nfa.h

--- DFAProver/DFAProver.cpp
+++ DFAProver/DFAProver.cpp
@@ -11,12 +11,11 @@ int main()
     nfa* a = nfa_read("2div.txt");
 	nfa* b = nfa_read("3div.txt");
 
-	nfa* c = nfa_intersect(a, b);
-	cout << sizeof(c->start);
 
+	//nfa_add(a, 0, 1, 0);
+	nfa* c = nfa_union(a, b);
+	nfa_to_dot(c, "2_2div.txt");
 	for (int i = 0; i < 100; i++) {
-		if (nfa_check(c, i)) {
-			cout << i << endl;
-		}
+		if (nfa_check(c, i)) cout << i << endl;
 	}
 }
