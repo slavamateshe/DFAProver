003bc573aa58f71486622619f939ad2f095a7c5b
Ivan Baranov <slavamateshe@yandex.ru>
2024-05-23 18:09:24 +0300

    automata added

--- DFAProver/DFAProver.cpp
+++ DFAProver/DFAProver.cpp
@@ -27,17 +27,5 @@ void test() {
 
 int main(void)
 {
-	nfa* a = nfa_linear_equals(2);
-	for (int i = 0; i < 20; i++) {
-		for (int j = 0; j < 20; j++) {
-			int input[2] = { i, j };
-			cout << i << " " << j << ": ";
-			if (nfa_check(a, input)) {
-				cout << "true " << endl;
-			}
-			else {
-				cout << "false" << endl;
-			}
-		}
-	}
+	cli();
 }
